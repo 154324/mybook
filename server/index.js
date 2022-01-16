79270f40c5027677412f9c3d2c9c2b7385d8ffc8
154324 <pkw961028@naver.com>
2022-01-16 18:22:35 +0900

    회원가입

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express()
 const port = 5000
+const {User} = require('./models/User')
+
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({extended:true}));
+
+//application/json
+app.use(express.json());
+
 
 const mongoose = require('mongoose')
 mongoose.connect("mongodb+srv://kwengwoo:4325118@firstnode.eq5cp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
@@ -9,5 +17,29 @@ mongoose.connect("mongodb+srv://kwengwoo:4325118@firstnode.eq5cp.mongodb.net/myF
 app.get('/',(req,res)=>{
  res.send("node welcome!")
 });
+
+
+app.post('/api/users/register',(req,res)=>{
+    //회원가입 할 때 필요한 정보들을 client 가져오면
+    //그것을 db에 넣어준다.
+
+    const user = new User(req,body)
+
+    //save는 mongo db에 함수이다.
+
+    user.save((err,doc)=>{
+        if(err) return res.json({success : false,err})
+        return res.status(200).json({
+            success:true
+        })
+
+    })
+
+
+
+
+})
+
+
 
 app.listen(port,()=>console.log(`express listening on port ${port}! `))
