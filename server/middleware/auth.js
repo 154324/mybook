8e2ca3c0524cebb03b1b341f494e7fd828902016
154324 <pkw961028@naver.com>
2022-01-16 21:56:49 +0900

    로그인api 구축

---
+++ server/middleware/auth.js
@@ -0,0 +1,35 @@
+const {User} = require('../models/User');
+
+
+
+let auth =(req,res,next)=>{
+
+
+    //인증처리 하는 곳
+
+    //클라이언트에서 쿠키에서 토큰을 가져온다
+
+    let token = req.cookies.x_auth;
+
+    User.findByToken(token,(err,user)=>{
+        if(err) throw err;
+        if(!user) return res.json({isAuth:false,error:true});
+
+        req.token = token;
+        req.user= user;
+        next();
+
+    })
+
+
+    //토큰을 복호화 한 후 유저를 찾느다
+
+    //유저가 있으면 인증 하고
+
+    //유저가 없으면 인증 no
+
+
+
+}
+
+module.exports={auth};
