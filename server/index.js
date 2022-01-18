4489819a5a3dfb741aa96c6de454b7cd62f4e699
154324 <pkw961028@naver.com>
2022-01-19 03:42:17 +0900

    로그인 리덕스 구현

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.post('/api/users/register',(req,res)=>{
 })
 
 
-app.post('/api/user/login',(req,res)=>{
+app.post('/api/users/login',(req,res)=>{
     //로그인 구현 
     //요청된 이메일을 데이터베이스에 있는지 찾는다.
     User.findOne({email : req.body.email},(err,user)=>{
@@ -58,7 +58,7 @@ app.post('/api/user/login',(req,res)=>{
     
   
     //요청된 이메일이 db에 있다면 비밀번호까지 맞는지 확인
-    user.comparePassword(req.body.Password,(err,isMatch)=>{
+    user.comparePassword(req.body.password,(err,isMatch)=>{
       if(!isMatch) return res.json({loginSuccess:false,message :"비밀번호가 틀렸습니다."})
     
   
--- server/models/User.js
+++ server/models/User.js
@@ -65,12 +65,14 @@ userSchema.pre('save',function(next){
 
 
 
-userSchema.methods.comparePassword = function(plainPassword,cb){
-    bcrypt.compare(plainPassword,this.Password,function(err,isMatch){
-        if(err)return cb(err);
-         cb(null,isMatch)
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+
+    //plainPassword 1234567    암호회된 비밀번호 $2b$10$l492vQ0M4s9YUBfwYkkaZOgWHExahjWC
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
     })
-   }
+}
    
    userSchema.methods.generateToken = function(cb){
     var user= this;
